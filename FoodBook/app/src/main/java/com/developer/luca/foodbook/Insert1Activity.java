dbc025e0e8e9bdeacb80b482c1ddd4f3a77b3c58
Luca Pussini <lucapussini@gmail.com>
2019-04-16 10:57:20 +0200

    Create Attività Base

---
+++ FoodBook/app/src/main/java/com/developer/luca/foodbook/Insert1Activity.java
@@ -0,0 +1,13 @@
+package com.developer.luca.foodbook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Insert1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insert1);
+    }
+}
--- FoodBook/app/src/main/java/com/developer/luca/foodbook/MainActivity.java
+++ FoodBook/app/src/main/java/com/developer/luca/foodbook/MainActivity.java
@@ -1,13 +1,67 @@
 package com.developer.luca.foodbook;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.preference.PreferenceManager;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fab = findViewById(R.id.floatingActionButton);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent insert_intent = new Intent(getApplicationContext(), Insert1Activity.class);
+                startActivity(insert_intent);
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case (R.id.action_preferred):
+                Intent preferred_intent = new Intent(this, ResultsActivity.class);
+                startActivity(preferred_intent);
+                break;
+
+            case (R.id.action_search):
+                Intent search_intent = new Intent(this, SearchActivity.class);
+                startActivity(search_intent);
+                break;
+
+            default:
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
