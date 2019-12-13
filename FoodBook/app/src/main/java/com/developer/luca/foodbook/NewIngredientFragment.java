0ae14572c7701806035becf9fa38ec117fadd762
Luca Pussini <lucapussini@gmail.com>
2019-12-13 15:46:04 +0100

    Spinner (dropdown) per scelta unità ingredienti

--- FoodBook/app/src/main/java/com/developer/luca/foodbook/NewIngredientFragment.java
+++ FoodBook/app/src/main/java/com/developer/luca/foodbook/NewIngredientFragment.java
@@ -7,12 +7,16 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 /**
  * Classe che gestisce il frammento di un singolo ingrediente
@@ -25,7 +29,7 @@ public class NewIngredientFragment extends Fragment {
 
     private EditText ingredientName_editText;
     private EditText ingredientQuantity_editText;
-    private Button unit_button;
+    private Spinner unit_spinner;
 
     private Ingredient ingredient;
 
@@ -52,28 +56,33 @@ public class NewIngredientFragment extends Fragment {
 
         ingredientName_editText = view.findViewById(R.id.ingredientName_editText);
         ingredientQuantity_editText = view.findViewById(R.id.ingredientQuantity_editText);
-        unit_button = view.findViewById(R.id.unit_button);
 
-        // Ogni volta che viene premuto il bottone cicla al unità di misura successiva
-        unit_button.setOnClickListener(new View.OnClickListener() {
-            @SuppressLint("SetTextI18n")
+        unit_spinner = view.findViewById(R.id.unit_spinner);
+
+        String[] choices = {getString(R.string.gr), getString(R.string.ml), getString(R.string.unit) };
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(mainActivity, R.layout.support_simple_spinner_dropdown_item, choices);
+
+        unit_spinner.setAdapter(adapter);
+
+        unit_spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onClick(View v) {
-                switch (unit_button.getText().toString()){
-                    case "UNITÀ":
-                        unit_button.setText(getString(R.string.gr));
-                        ingredient.setUnit(Ingredient.Unit.GR);
+            // Imposta l'unità di misura del ingrediente in base al elemento selezionato
+            // Viene chiamato anche appena viene creato
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                switch (position){
+                    case 0: ingredient.setUnit(Ingredient.Unit.GR);
                         break;
-                    case "GR":
-                        unit_button.setText(getString(R.string.ml));
-                        ingredient.setUnit(Ingredient.Unit.ML);
+                    case 1: ingredient.setUnit(Ingredient.Unit.ML);
                         break;
-                    case "ML":
-                        unit_button.setText(R.string.unit);
-                        ingredient.setUnit(Ingredient.Unit.UNIT);
+                    case 2: ingredient.setUnit(Ingredient.Unit.UNIT);
+                        break;
                 }
+                hideSoftKeyboard(mainActivity, view);
+            }
 
-                hideSoftKeyboard(mainActivity, v);
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
             }
         });
     }
